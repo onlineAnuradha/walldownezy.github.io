2f96af50aa6a6a777a7c38304c07c4d179bdc5b1
onlineAnuradha <onlineanuradha100@gmail.com>
2025-02-07 23:38:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<?php
-echo "Welcome to my wallpaper site!";
-?>
-
+<div class="gallery">
+    <?php
+    $wallpapers = glob('uploads/*.{jpg,jpeg,png,gif,webp}', GLOB_BRACE);
+    foreach ($wallpapers as $wallpaper) {
+        echo "
+        <div class='wallpaper'>
+            <img src='$wallpaper' alt='Wallpaper'>
+            <a href='$wallpaper' download>Download</a>
+            <button class='like-btn'>❤</button>
+        </div>";
+    }
+    ?>
+</div>
